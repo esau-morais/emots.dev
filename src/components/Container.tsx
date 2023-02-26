3320f3f060de771f38221f9edf88008583eae333
esau-morais <esaumorais7@gmail.com>
2023-02-26 14:01:59 -0300

    feat(analytics): add `@vercel/analytics` to project

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,10 +3,14 @@
 import type { ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast'
 
+import { Analytics } from '@vercel/analytics/react'
+
 export const Container = ({ children }: { children: ReactNode }) => {
   return (
     <main className="mx-auto grid max-w-3xl grid-cols-6 gap-6 px-6 pb-40 pt-16">
       {children}
+
+      <Analytics />
 
       <Toaster />
     </main>
