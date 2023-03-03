83e3b6c7beb7db797f73f707d420989587c4e730
esau-morais <esaumorais7@gmail.com>
2023-03-03 15:14:01 -0300

    feat: add work type

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -3,4 +3,5 @@ export const getPageMetadata = (page: any) => ({
   cover: page.properties.Cover.files[0]?.file.url,
   title: page.properties.Title.title[0].plain_text,
   slug: page.properties.Slug.rich_text[0].plain_text,
+  type: page.properties.Type.select.name,
 })
