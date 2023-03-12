90497b4fbd26c9867a4ac22a82d67999b8dd345c
esau-morais <esaumorais7@gmail.com>
2023-03-11 23:18:56 -0300

    feat: redirect to subdomain when accessing subpath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   experimental: {
     appDir: true,
@@ -7,6 +6,15 @@ const nextConfig = {
   },
   images: {
     domains: ['localhost', 'emots.dev', 's3.us-west-2.amazonaws.com'],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/links',
+        destination: 'https://links.emots.dev',
+        permanent: true,
+      },
+    ]
   },
 }
 
