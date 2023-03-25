d6b3d64f5eecf7f724fb6d2882f1e720fbc688ee
esau-morais <esaumorais7@gmail.com>
2023-03-25 12:33:49 -0300

    feat: generate `sitemap.xml`

---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import { findAllWorks } from '@/lib/fetch'
+import { url } from '@/utils/consts'
+
+const sitemap = async () => {
+  const works = await findAllWorks()
+  const allWorks = works?.map((work) => ({
+    url: `${url}/work/${work.slug}`,
+    lastModified: work.editedAt,
+  })) as { url: string; lastModified: string }[]
+
+  const routes = ['', '/works'].map((route) => ({
+    url: `${url}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes, ...allWorks]
+}
+
+export default sitemap
--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,6 +1,40 @@
 import { BASE_URL } from '@/utils/consts'
+import { getPageMetadata } from '@/utils/metadata'
+import { Client } from '@notionhq/client'
 
-import type { Work } from './types/work'
+import { env } from './env'
+import type { Work, WorkMetadata } from './types/work'
+
+const { NOTION_KEY, DATABASE_ID } = env
+
+const notion = new Client({ auth: NOTION_KEY })
+
+export const findAllWorks = async () => {
+  try {
+    const response = await notion.databases.query({
+      database_id: DATABASE_ID,
+      filter: {
+        property: 'Status',
+        select: {
+          equals: 'Published',
+        },
+      },
+      sorts: [
+        {
+          property: 'Created at',
+          direction: 'ascending',
+        },
+      ],
+    })
+
+    const allWorks = response.results
+    return allWorks.map((singleWork: unknown) => ({
+      ...getPageMetadata(singleWork),
+    })) as WorkMetadata[]
+  } catch (error) {
+    if (error instanceof Error) console.error(error)
+  }
+}
 
 export const findSingleWorkBySlug = async (slug: string) => {
   const res = await fetch(`${BASE_URL}/api/work?slug=${slug}`)
