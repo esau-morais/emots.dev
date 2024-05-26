3625a2a29db92c2a46daf43bf8e71f2d8c9742da
esau-morais <esaumorais7@gmail.com>
2024-05-25 21:10:12 -0300

    fix: typo in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,7 +74,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         </main>
         <Footer />
         <Toaster />
-        <Analytics />;
+        <Analytics />
       </body>
     </html>
   )
