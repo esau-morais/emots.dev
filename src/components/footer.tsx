54eba2ec7e9022de202c55dc098b970e55a81b96
esau-morais <esaumorais7@gmail.com>
2026-02-05 19:16:00 -0300

    fix: set height correctly due to safe-area

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,10 +11,13 @@ export function Footer({ children }: { children: ReactNode }) {
 	return (
 		<footer
 			className={cn(
-				"font-mono fixed inset-x-0 bottom-0 shadow-[0_-20px_30px_-10px_rgba(0,0,0,0.9)] mx-auto flex h-10 max-w-3xl items-center justify-between border-t border-gray-800 bg-black/80 px-4 text-sm text-gray-500 backdrop-blur-sm md:px-8 before:absolute before:left-4 before:-top-6 before:hidden before:h-6 before:w-px before:bg-gray-800 after:absolute after:right-4 after:-top-6 after:hidden after:h-6 after:w-px after:bg-gray-800 md:before:left-8 md:before:-top-8 md:before:block md:before:h-8 md:after:right-8 md:after:-top-8 md:after:block md:after:h-8",
+				"font-mono fixed inset-x-0 bottom-0 shadow-[0_-20px_30px_-10px_rgba(0,0,0,0.9)] mx-auto flex max-w-3xl items-center justify-between border-t border-gray-800 bg-black/80 px-4 text-sm text-gray-500 backdrop-blur-sm md:px-8 before:absolute before:left-4 before:-top-6 before:hidden before:h-6 before:w-px before:bg-gray-800 after:absolute after:right-4 after:-top-6 after:hidden after:h-6 after:w-px after:bg-gray-800 md:before:left-8 md:before:-top-8 md:before:block md:before:h-8 md:after:right-8 md:after:-top-8 md:after:block md:after:h-8",
 				isAnimating ? "z-[10000]" : "z-40",
 			)}
-			style={{ paddingBottom: "env(safe-area-inset-bottom, 0px)" }}
+			style={{
+				height: "calc(2.5rem + env(safe-area-inset-bottom, 0px))",
+				paddingBottom: "env(safe-area-inset-bottom, 0px)",
+			}}
 		>
 			{children}
 		</footer>
